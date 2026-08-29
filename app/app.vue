f879731eaddf4987a33ef0839fdf8e8631aaf3c3
zybixx84-design <zybixx84@gmail.com>
2026-08-29 23:08:53 +0100

    Setup Tailwind & Daisy

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-secondary">
+      View Map <Icon name="tabler:map" size="32" />
+    </button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,25 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+
+  devtools: {
+    enabled: true,
+  },
+
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+
+  css: [
+    "~/assets/css/main.css",
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
   eslint: {
     config: {
       standalone: false,
