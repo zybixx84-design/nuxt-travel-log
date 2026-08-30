18bfe502203f65e452f9bb48150e923552e0ead0
zybixx84-design <zybixx84@gmail.com>
2026-08-30 15:51:23 +0100

    Landing Page

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-secondary">
-      View Map <Icon name="tabler:map" size="32" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
