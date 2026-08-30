1372de07442b94989c54a53eeda600ff0ae8af79
zybixx84-design <zybixx84@gmail.com>
2026-08-30 16:55:10 +0100

    Theme toggle

---
+++ app/components/app/themeToggle.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value: boolean) {
+    colorMode.preference = value ? "dark" : "light";
+  },
+});
+</script>
+
+<template>
+  <label class="swap mx-4 swap-rotate">
+    <input v-model="isDark" type="checkbox">
+
+    <!-- sun icon -->
+    <Icon
+      class="swap-on"
+      name="tabler:sun-filled"
+      size="24"
+    />
+
+    <!-- moon icon -->
+    <Icon
+      class="swap-off"
+      name="tabler:moon-filled"
+      size="24"
+    />
+  </label>
+</template>
