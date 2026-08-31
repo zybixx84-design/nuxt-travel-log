2ede6318c7ba36dbfbcc5029538f3d83ff6a82bc
zybixx84-design <zybixx84@gmail.com>
2026-08-31 15:19:34 +0100

    Auth setup

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,12 +1,15 @@
 import { integer, real, snakeCase, timestamp, varchar } from "drizzle-orm/pg-core";
 
-export const location = snakeCase.table("location", {
+import { user } from "./auth-schema";
+
+export const location = snakeCase.table ("location", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   slug: varchar({ length: 255 }).notNull().unique(),
   description: varchar({ length: 255 }).notNull(),
   latitude: real().notNull(),
   longitude: real().notNull(),
+  userId: integer().notNull().references(() => user.id),
   createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
   updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow().$onUpdate(() => new Date()),
 });
@@ -20,6 +23,7 @@ export const locationLog = snakeCase.table("locationLog", {
   latitude: real().notNull(),
   longitude: real().notNull(),
   locationId: integer().notNull().references(() => location.id),
+  userId: integer().notNull().references(() => user.id),
   createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
   updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow().$onUpdate(() => new Date()),
 });
@@ -28,6 +32,7 @@ export const locationLogImage = snakeCase.table("locationLogImage", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   key: varchar({ length: 255 }).notNull(),
   locationLogId: integer().notNull().references(() => locationLog.id),
+  userId: integer().notNull().references(() => user.id),
   createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
   updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow().$onUpdate(() => new Date()),
 });
