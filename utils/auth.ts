ecff9f6b80fb5d12be69e6e2302c92e58345252e
zybixx84-design <zybixx84@gmail.com>
2026-08-31 21:46:11 +0100

    Var change

--- utils/auth.ts
+++ utils/auth.ts
@@ -19,8 +19,8 @@ export const auth = betterAuth({
 
   socialProviders: {
     github: {
-      clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET,
+      clientId: env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: env.AUTH_GITHUB_CLIENT_SECRET,
     },
   },
 });
